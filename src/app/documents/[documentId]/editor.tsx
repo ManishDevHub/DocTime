9c7e5553cdac964f8cfb033d842b222a04db7fe0
YourGitHubUsername <ramuk13476@gmail.com>
2025-11-10 01:52:36 +0530

    styleing editor-page

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -8,7 +8,8 @@ export default function Editor() {
      const editor = useEditor({
         editorProps:{
             attributes:{
-                class: "focus:outline-none"
+                style: "padding-left: 56px; padding-right: 56px;",
+                class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 cursor-text"
             },
 
         },
@@ -16,8 +17,10 @@ export default function Editor() {
     content: '<p>Hello World! 🌎️</p>',
      })
   return (
-    <div>
-        <EditorContent  editor={editor}/>
+    <div className='size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print: bg-white print:overflow-visible'>
+       <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
+        <EditorContent className=''  editor={editor}/>
+        </div>
     </div>
   )
 }
--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -10,12 +10,12 @@ const DocumentIdPage = async ({ params}: DocumentIdPageProps) => {
   const {documentId} = await params;
 
   return (
-    <div className=" size-full overflow-x-auto bg-[#F9FBFD] px-4 print: p-0 print:bg-white print: overflow-visible">
-     <div className="min-w-max flex justify-center w-[816px] py-4 print: p-0 mx-auto print:w-full print:min-w-0">
+    
+     <div className="min-h-screen bg-[#FAFBFD]">
 
       <Editor />
 
-      </div>
+      
     </div>
   )
 }
