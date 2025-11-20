c6cbbb368a2c8afdad94b861e1269716edfdfec7
YourGitHubUsername <ramuk13476@gmail.com>
2025-11-20 23:52:54 +0530

    .

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -75,13 +75,16 @@ setEditor(editor);
        Underline,
        
     ],
-    content: '<p>Hello World! 🌎️</p>',
+    content: '<p>Hello World! this is my first project of webdev 🌎️</p>',
      })
      
   return (
     <div className='size-full overflow-x-auto bg-[#dfe0e2] px-4 print:p-0 print: bg-white print:overflow-visible'>
        <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
         <EditorContent className=''  editor={editor}/>
+
+
+        
         </div>
     </div>
   )
